6a24355366aaf02748de44c6efc5f2fdb1ee0d20
elo <evsenik23@mail.ru>
2025-03-23 13:28:25 +0300

    now not a tg mini app

--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-
-import { init, miniApp } from '@telegram-apps/sdk';
-
-
-const initializeTelegramSDK = async () => {
-    try {
-        await init();
-
-
-        if (miniApp.ready.isAvailable()) {
-            await miniApp.ready();
-            console.log('Mini App готово');
-        }
-
-
-    } catch (error) {
-        console.error('Ошибка инициализации:', error);
-    }
-};
-
-
-initializeTelegramSDK();
-
-
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <App />
